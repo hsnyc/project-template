79572a1bf9bdd40da7a5e4e1479ccce8752781f3
Henry <hsanchez@healthesystems.com>
2018-04-18 16:50:07 -0400

    style and content updates.

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -1,5 +1,54 @@
 /* =============================
 This file contains all custom 'home brewed' scripts.
     ============================= */
+// NAV Menu Drop Downs
 
+var navMenu = document.querySelector('.nav-menu');
+
+navMenu.addEventListener('click', dropMenu , false);
+
+function dropMenu(e) {
+    //console.log(e);
+
+    //check for children
+    if(e.target.classList.contains('menu') && e.target.children.length !== 0) {
+        
+        //spin icon
+        e.target.children[0].classList.toggle('spin');
+
+        //show menu
+        e.target.children[1].classList.toggle('show-menu');
+    }
+    
+    //check for icon click
+    if (e.target.classList.contains('menu-icon')) {
+
+        //spin icon
+        e.target.classList.toggle('spin');
+
+        //show menu
+        e.target.nextElementSibling.classList.toggle('show-menu');
+    }
+
+    e.stopPropagation();
+};
+
+window.addEventListener('mouseup', hideMenu , false);
+
+function hideMenu(e) {
+    //console.log(e);
+
+    var menu = document.querySelectorAll('.menu');
+
+    for(var i = 0; i < menu.length; i++){
+        if(e.target !== menu[i] && e.target !== menu[i].children[0]) {
+            if(menu[i].children.length !== 0){
+                menu[i].children[0].classList.remove('spin');
+                menu[i].children[1].classList.remove('show-menu');
+                //console.log(menu[i]);
+            } 
+        }
+    }
+    e.stopPropagation();
+}
 
