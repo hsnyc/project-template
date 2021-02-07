c525cb7ce97f8def58efaf48cf3d98974fb309e4
Henry <hsnyc@live.com>
2021-02-07 16:50:52 -0500

    adding html task to scss watch task in order to run cacheBust

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ function watchTask(){
 
   //now watch each file
   watch(files.htmlPath, htmlTask).on('change', browserSync.reload);
-  watch(files.scssPath, scssTask).on('change', browserSync.reload);
+  watch(files.scssPath, series(scssTask, htmlTask)).on('change', browserSync.reload);
   watch(files.jsPath, jsTask).on('change', browserSync.reload);
   watch(files.imgPath, imgTask).on('change', browserSync.reload);
 }
